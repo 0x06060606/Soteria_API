7fa7e72e1b0bb465f58f1b534362d40a6f761a27
John Bell <johnthegod@mail.ru>
2019-12-08 21:19:51 -0500

    Add files via upload

--- api.py
+++ api.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, Markup
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps, loads, dumps
 from re import search
 from random import randint
 db_connect = create_engine('sqlite:///proxies.db')
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./html')
 api = Api(app)
+@app.route('/')
+def index():
+    return render_template('home.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 class http(Resource):
     def get(self):
         conn = db_connect.connect()
